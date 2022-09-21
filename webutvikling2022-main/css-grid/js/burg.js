5522bd8282644c3e7a43cda83fb0eefb16458423
Muhsin310302 <Muhsin310302@hotmail.com>
2022-09-21 11:48:08 +0200

    ps

---
+++ webutvikling2022-main/css-grid/js/burg.js
@@ -0,0 +1,18 @@
+
+const menu = documnet.getElementById("tilgang");
+
+const hamMenu = () => {
+
+    if(menu.style.display == "block")  {
+        menu.style.display ="none";
+    } else {
+        menu.style.display = "block"
+    }
+
+}
+
+
+
+
+
+
